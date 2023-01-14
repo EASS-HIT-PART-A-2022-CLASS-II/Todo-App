51035f4fc290a1a6b911fa265610e9f60795630a
Peleg Levy <{ID}+{username}@users.noreply.github.com>
2023-01-14 23:42:27 +0200

    update frontend container

--- app/backend/models/model.py
+++ app/backend/models/model.py
@@ -2,14 +2,15 @@ from pydantic import BaseModel
 import datetime
 
 class Task(BaseModel):
+    tag: str
     description: str
-    creation_date: str = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-    check = 0
+    creation_date: str = datetime.datetime.now().strftime("%d/%m/%Y")
+    check: bool = 0
 
 
 def MessageResponse(data, message):
     return {
-        "data": [data],
+        "data": data,
         "code": 200,
         "message": message,
     }
@@ -26,11 +27,17 @@ def MessageError(data,message):
 def taskEntity(task)-> dict:
     return {
         "id": str(task["_id"]),
+        "tag": task["tag"],
         "description": task["description"],
         "creation_date": task["creation_date"],
-        "check": task["check"]
+        "check": task["check"],
     }
 
 
 def tasksEntity(entity)-> list:
     return [taskEntity(task) for task in entity]
+
+def TestResponse(data):
+    return {
+        "data": data
+    }
--- app/backend/routers/router.py
+++ app/backend/routers/router.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, APIRouter
-from backend.models.model import (Task, MessageResponse, MessageError, taskEntity, tasksEntity)
+from backend.models.model import (Task, MessageResponse, MessageError, taskEntity, tasksEntity,TestResponse)
 from backend.database.db_mongo import tasks_db
 from bson import ObjectId
 from typing import List
@@ -7,12 +7,29 @@ from typing import List
 router = APIRouter()
 
 #Home Page
-@router.get("/" , tags=["Manage Tasks"])
+@router.get("/v1/tasks", tags=["Manage Tasks"])
 async def get_all_tasks():
     all_tasks = tasksEntity(tasks_db.find())
     if all_tasks:
         return MessageResponse(all_tasks,"Retrive all tasks from database.")
     return MessageError("Error","Unable to found tasks.")
+
+
+#Get all tags
+@router.get("/v1/tags", tags=["Manage Tasks"])
+async def get_all_tags():
+    tags = tasks_db.distinct('tag')
+    if tags:
+        return MessageResponse(tags,"Retrive all tags")
+    return MessageError("Error:","Unable to found tags")
+
+
+@router.get("/v1/tags/{tag}",  tags=["Manage Tasks"])
+async def get_all_task_by_specific_tag(tag):
+    tasks = tasksEntity(tasks_db.find({"tag": tag}))
+    if tasks:
+        return MessageResponse(tasks,"Tasks by tag: {}".format(tag))
+    return MessageError("Error:","Unable to fount task with tag: {}".format(tag))
 
 
 #Get task by task id
@@ -36,17 +53,17 @@ async def create_new_task(task: Task):
 
 #Update Existing Task
 @router.put("/v1/task/{id}", tags=["Manage Tasks"])
-async def update_existing_task(id, task: Task):
+async def update_existing_task(id, task:Task):
     task_update = tasks_db.find_one_and_update({"_id":ObjectId(id)},{"$set":dict(task)})
     if task_update:
         return MessageResponse(tasksEntity(tasks_db.find({"_id":ObjectId(id)})),"Task Updated.")
     else:
-        return MessageError("Error","Unable to update task id: {}".format(id))
+        return MessageError("Error","Unable to update task id: {}".format(id)) 
 
 
 #Delete Existing Task
 @router.delete("/v1/task/{id}", tags=["Manage Tasks"])
-async def delete_task(id, task: Task):
+async def delete_task(id):
     delete_task = tasks_db.find_one_and_delete({"_id":ObjectId(id)})
     if delete_task:
         return MessageResponse("Task ID: {} deleted successfully.".format(id),"Task Deleted.")
@@ -55,6 +72,6 @@ async def delete_task(id, task: Task):
 
 #test
 @router.post("/v1/test_app", tags=["Manage Tests"])
-async def create_new_task(task :Task):
+async def create_new_task(task):
     return task
 
